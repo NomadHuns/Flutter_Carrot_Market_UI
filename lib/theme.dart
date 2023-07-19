a789badb2db9f80d703e3fe7ab76b47896838d17
nomadhuns <kimalss22@gmail.com>
2023-07-19 11:08:30 +0900

    텍스트 테마 수정

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,12 +3,12 @@ import 'package:google_fonts/google_fonts.dart';
 
 TextTheme textTheme() {
   return TextTheme(
-    displayMedium: GoogleFonts.openSans(fontSize: 18.0, color: Colors.black),
-    displaySmall: GoogleFonts.openSans(
+    displayLarge: GoogleFonts.openSans(fontSize: 18.0, color: Colors.black),
+    displayMedium: GoogleFonts.openSans(
         fontSize: 16.0, color: Colors.black, fontWeight: FontWeight.bold),
-    bodyMedium: GoogleFonts.openSans(fontSize: 16.0, color: Colors.black),
-    bodySmall: GoogleFonts.openSans(fontSize: 14.0, color: Colors.grey),
-    titleSmall: GoogleFonts.openSans(fontSize: 15.0, color: Colors.black),
+    bodyLarge: GoogleFonts.openSans(fontSize: 16.0, color: Colors.black),
+    bodyMedium: GoogleFonts.openSans(fontSize: 14.0, color: Colors.grey),
+    titleMedium: GoogleFonts.openSans(fontSize: 15.0, color: Colors.black),
   );
 }
 
