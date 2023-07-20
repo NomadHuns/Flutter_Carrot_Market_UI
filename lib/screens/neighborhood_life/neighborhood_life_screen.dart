e2bc920dece9245029ed2d893466df8140a0d9a6
nomadhuns <kimalss22@gmail.com>
2023-07-20 10:53:36 +0900

    동네 생활 페이지 구현

--- lib/screens/neighborhood_life/neighborhood_life_screen.dart
+++ lib/screens/neighborhood_life/neighborhood_life_screen.dart
@@ -1,3 +1,8 @@
+import 'package:carrot_market_ui/models/neighborhood_life.dart';
+import 'package:carrot_market_ui/screens/components/appbar_preferred_size.dart';
+import 'package:carrot_market_ui/screens/neighborhood_life/components/life_body.dart';
+import 'package:carrot_market_ui/screens/neighborhood_life/components/life_header.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NeighborhoodLifeScreen extends StatelessWidget {
@@ -5,8 +10,33 @@ class NeighborhoodLifeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('neighborhoodLifeScreen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('동네생활'),
+        actions: [
+          IconButton(icon: const Icon(CupertinoIcons.search), onPressed: () {}),
+          IconButton(
+              icon: const Icon(CupertinoIcons.plus_rectangle_on_rectangle),
+              onPressed: () {}),
+          IconButton(icon: const Icon(CupertinoIcons.bell), onPressed: () {}),
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          const LifeHeader(),
+          ...List.generate(
+            neighborhoodLifeList.length,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: LifeBody(
+                neighborhoodLife: neighborhoodLifeList[index],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
